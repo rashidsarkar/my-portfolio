4071a26ab2abdceb9dd8765ddc9312969c979d2b
rashidsarkar <rashidrock558@gmail.com>
2023-12-11 20:55:24 +0600

    Update dependencies and add react-type-animation

--- src/Components/Hero/HeroSection.jsx
+++ src/Components/Hero/HeroSection.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import "./herocss.css";
+import TextScramble from "../Shared/TextScramble/TextScramble";
+import { TypeAnimation } from "react-type-animation";
 const HeroSection = () => {
   return (
     <section
@@ -12,10 +14,26 @@ const HeroSection = () => {
       <div className="container">
         <div className="row justify-content-md-center">
           <div className="col-12 col-md-11 col-lg-9 col-xl-7 col-xxl-6 text-center text-white">
-            <h1 className="display-5 fw-bold mb-3">
+            <h1 data-aos="fade-right" className="display-5 fw-bold mb-3">
               Elevate Your Web Presence
             </h1>
-            <p className="lead mb-5">
+            {/* <TypeAnimation
+              sequence={[
+                "Experience Modern Luxury Living!",
+                1000,
+                "Explore Unmatched Comfort and Style!",
+                1000,
+                "Offers Modern Living Spaces!",
+                1000,
+                "Discover a New Perspective!",
+                1000,
+              ]}
+              speed={50}
+              style={{ fontSize: "3rem", fontWeight: "700" }}
+              repeat={Infinity}
+            /> */}
+
+            <p data-aos="fade-left" className="lead mb-5 text-white ">
               Transform your online presence and stand out from the rest with
               our cutting-edge solutions.
             </p>
